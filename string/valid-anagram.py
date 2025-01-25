d22034dfbf1a222faef2fbd5403f59904ac64d0b
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-01-24 18:57:34 -0600

    LC-valid-anagram [Runtime: 9 ms; Memory: 17.8 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,21 +1,26 @@
-#hash table 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!= len(t):
-            return False
-        countS, countT = {}, {}
+        # Counter is a data structure which is a hashmap that counts the occurences itself, so the sol below can be written in one line
+        return Counter(s) == Counter(t)
 
-        #Formation of hashmaps counting occurences of each character in both strings
-        #we're using get() to avoid key error, so that if the key doesn't exist we return 0
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countT.get(t[i],0)
+# #hash table 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s)!= len(t):
+#             return False
+#         countS, countT = {}, {}
+
+#         #Formation of hashmaps counting occurences of each character in both strings
+#         #we're using get() to avoid key error, so that if the key doesn't exist we return 0
+#         for i in range(len(s)):
+#             countS[s[i]] = 1 + countS.get(s[i],0)
+#             countT[t[i]] = 1 + countT.get(t[i],0)
         
-        #Checking if occurence of each charachter(which is value here) and each chcaracter(which is key here) are equal in both strings, if not return false
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
-                return False
-        return True
+#         #Checking if occurence of each charachter(which is value here) and each chcaracter(which is key here) are equal in both strings, if not return false
+#         for c in countS:
+#             if countS[c] != countT.get(c, 0):
+#                 return False
+#         return True
 
 
 
