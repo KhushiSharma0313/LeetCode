363a86abc632a6638418df7e150f6b0252369d70
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-01-24 18:32:36 -0600

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,13 +1,15 @@
 #hash table 
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return set(s) == set(t)
 
 #brute force and sorting
 #time complexity = O(nlogn+ mlogm)
 #space complexity = O(1) or O(n) depending on sorting technique
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False 
         
-        return sorted(s) == sorted(t)
+#         return sorted(s) == sorted(t)
         
