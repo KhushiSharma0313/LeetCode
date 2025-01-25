f8f33498b9bcc39b990faa17bc4e7553a0a63ca9
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-01-24 18:55:40 -0600

    LC-valid-anagram [Runtime: 11 ms; Memory: 18 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -3,7 +3,22 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!= len(t):
             return False
-        return set(s) == set(t)
+        countS, countT = {}, {}
+
+        #Formation of hashmaps counting occurences of each character in both strings
+        #we're using get() to avoid key error, so that if the key doesn't exist we return 0
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        
+        #Checking if occurence of each charachter(which is value here) and each chcaracter(which is key here) are equal in both strings, if not return false
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+
+
+
 
 #brute force and sorting
 #time complexity = O(nlogn+ mlogm)
