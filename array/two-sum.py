4ff8b3aa7ebe2eddd46ffa7925b4c14c98f9ca36
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-01-26 21:47:58 -0600

    LC-two-sum [Runtime: 0 ms; Memory: 18.9 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,18 +1,33 @@
+# # one pass approach
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         # approach should be that 
+#         # to find diff for each number in array
+#         # make a hashmap of value to their index
+#         # then lookup that number in hashmap of the array, if it exists append to array, 
+#         # if not, add that number to hashmap and move on to next number
+#         # time complexity = O(n)
+#         # space complexity = O(n)
+#         result = {} # val: index
+#         for i in range(len(nums)):
+#             diff = target - nums[i]
+#             if diff in result:
+#                 return [result[diff], i]
+#             result[nums[i]] = i
+#         return
+
+# two pass approach 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # approach should be that 
-        # to find diff for each number in array
-        # then lookup that number in hashmap of the array, if it exists append to array, 
-        # if not move on to next number
-        result = {} # val: index
+        result = {}
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in result:
-                return [result[diff], i]
             result[nums[i]] = i
+        
+        for j in range(len(nums)):
+            diff = target - nums[j]
+            if diff in result and result[diff] !=j:
+                return [result[diff], j]
         return
-
-
 
 
 
