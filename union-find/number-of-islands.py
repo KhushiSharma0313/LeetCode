d55835c9000acc60d46e58c80c0fc29b6d9684d0
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-03-18 01:28:38 -0500

    LC-number-of-islands [Runtime: 333 ms; Memory: 25.6 MB]

---
+++ union-find/number-of-islands.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        #when we visited island 1st, we need to look for its adjacent piece being land too 
+        # for that we'll use bfs 
+        #
+        if not grid: # if it's not a grid
+            return 0
+        
+        rows, cols = len(grid), len(grid[0]) #defining rows and cols 
+        visit = set() #for visited elements 
+        islands = 0 #to count n.o of islands 
+
+        def bfs(r,c): #iterative algo
+            q = collections.deque() #declaring a queue, since we need data struc to store visited nodes
+            visit.add((r,c)) #add the node in visit set 
+            q.append((r,c)) #add node in queue too 
+
+            while q: #while q is not empty
+                row, col = q.popleft() #if we do pop instead of pop left it will become dfs, iterative 
+                directions = [[1,0],[-1,0],[0,1],[0,-1]] #dir for right, left, up, down 
+                for dr, dc in directions: 
+                    r = row+dr
+                    c= col+dc
+                    if(r in range(rows) and
+                       c in range(cols) and
+                       grid[r][c] == "1" and
+                       (r,c) not in visit):
+                       q.append((r,c))
+                       visit.add((r,c)) 
+
+        for r in range(rows): #iterate r in rows
+            for c in range(cols): #iterate c in cols
+                if(grid[r][c] == "1") and (r,c) not in visit: #if its a land and if it hasn't been visited already 
+                    bfs(r,c) # run bfs algorithm on it 
+                    islands +=1 #iterate through the loop 
+        return islands 
+
+        
