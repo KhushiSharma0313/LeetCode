34bbb45bdcbccfced15f2aacb414a8f253a7a46e
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-04-26 00:17:30 -0500

    LC-number-of-islands [Runtime: 340 ms; Memory: 25.5 MB]

--- depth-first-search/number-of-islands.py
+++ depth-first-search/number-of-islands.py
@@ -1,38 +1,82 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #when we visited island 1st, we need to look for its adjacent piece being land too 
-        # for that we'll use bfs 
-        #
-        if not grid: # if it's not a grid
-            return 0
+        # we go to 1, then look in all direction, if in any direction it has 1, we go there and so on
+        # when no direction has 1, it's one island
+        # then we look at remaining 1s
+        # to not lose track of islands and reount, we'll store the visited ones in a set 
+        # it's basically dfs 
+
+        island = 0
+        visited = set() # for 1s that are visited 
+        rows = len(grid)
+        cols = len(grid[0])
+
+ #helper function dfs, taking coordinates as input 
+        def dfs(r,c):
+            if (r,c) in visited:
+                return 
+            visited.add((r,c))
+            directions = [[1,0],[0,1],[-1,0],[0,-1]] # up, right, down, left
+
+            # dr to move in row, dc to move in col 
+            for dr, dc in directions:
+                nr = dr + r # adding direction in row coord, moving up or down 
+                nc = dc + c # adding direction in col coord, moving  left or right 
+
+                if(nr in range(rows) and nc in range(cols) and grid[nr][nc] == "1"):
+                    dfs(nr,nc) #running dfs recursivley until all 1s visited 
+
+        # point to point and run dfs if it's 1 and not visited
+        for r in range(rows):
+            for c in range(cols):
+                if((r,c) not in visited and grid[r][c] == "1"):
+                    dfs(r,c)
+                    island +=1
+
+        return island
+
+
+
+
+
+
         
-        rows, cols = len(grid), len(grid[0]) #defining rows and cols 
-        visit = set() #for visited elements 
-        islands = 0 #to count n.o of islands 
 
-        def bfs(r,c): #iterative algo
-            q = collections.deque() #declaring a queue, since we need data struc to store visited nodes
-            visit.add((r,c)) #add the node in visit set 
-            q.append((r,c)) #add node in queue too 
 
-            while q: #while q is not empty
-                row, col = q.popleft() #if we do pop instead of pop left it will become dfs, iterative 
-                directions = [[1,0],[-1,0],[0,1],[0,-1]] #dir for right, left, up, down 
-                for dr, dc in directions: 
-                    r = row+dr
-                    c= col+dc
-                    if(r in range(rows) and
-                       c in range(cols) and
-                       grid[r][c] == "1" and
-                       (r,c) not in visit):
-                       q.append((r,c))
-                       visit.add((r,c)) 
+        # #when we visited island 1st, we need to look for its adjacent piece being land too 
+        # # for that we'll use bfs 
+        # #
+        # if not grid: # if it's not a grid
+        #     return 0
+        
+        # rows, cols = len(grid), len(grid[0]) #defining rows and cols 
+        # visit = set() #for visited elements 
+        # islands = 0 #to count n.o of islands 
 
-        for r in range(rows): #iterate r in rows
-            for c in range(cols): #iterate c in cols
-                if(grid[r][c] == "1") and (r,c) not in visit: #if its a land and if it hasn't been visited already 
-                    bfs(r,c) # run bfs algorithm on it 
-                    islands +=1 #iterate through the loop 
-        return islands 
+        # def bfs(r,c): #iterative algo
+        #     q = collections.deque() #declaring a queue, since we need data struc to store visited nodes
+        #     visit.add((r,c)) #add the node in visit set 
+        #     q.append((r,c)) #add node in queue too 
+
+        #     while q: #while q is not empty
+        #         row, col = q.popleft() # pop leftmost cell from queue
+        #          #if we do pop instead of pop left it will become dfs, iterative 
+        #         directions = [[1,0],[-1,0],[0,1],[0,-1]] #dir for right, left, up, down 
+        #         for dr, dc in directions: # dr and dc are 1 and 2nd value of each direction pair
+        #             r = row+dr #move in rows
+        #             c= col+dc #move in cols 
+        #             if(r in range(rows) and #check if row is in bound 
+        #                c in range(cols) and #check if col is in bound 
+        #                grid[r][c] == "1" and #check if it's land 
+        #                (r,c) not in visit): #if its visited or not 
+        #                q.append((r,c))
+        #                visit.add((r,c)) 
+
+        # for r in range(rows): #iterate r in rows
+        #     for c in range(cols): #iterate c in cols
+        #         if(grid[r][c] == "1") and (r,c) not in visit: #if its a land and if it hasn't been visited already 
+        #             bfs(r,c) # run bfs algorithm on it 
+        #             islands +=1 #iterate through the loop 
+        # return islands 
 
         
