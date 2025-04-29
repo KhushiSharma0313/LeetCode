61914750d0a1dd257173d755488e8c89fe20d745
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-04-29 12:39:43 -0500

    LC-house-robber [Runtime: 0 ms; Memory: 17.9 MB]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -16,13 +16,13 @@ class Solution:
 
         #base cases
         dp[0] = nums[0]
-        dp[1] = nums[1]
+        dp[1] = max(nums[0],nums[1])
 
         for i in range(2,n):
             dp[i] = max(nums[i] + dp[i-2], dp[i-1])
         
         #return last element of dp
-        return dp[n]
+        return dp[n-1]
         
 
 
