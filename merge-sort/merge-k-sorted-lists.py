a56754eb441e7fd2cc61e9bc3ed319a026f7044e
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-05-02 17:52:31 -0500

    LC-merge-k-sorted-lists [Runtime: 13 ms; Memory: 20.1 MB]

---
+++ merge-sort/merge-k-sorted-lists.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        #for linked list with only one node, imagine a merge sort/divide and conquer type algo 
+        #brute force 
+        #first sort first two node, then add one and sort it and so on
+        # this is not super effiecient, as we have k lists and every time we're merging it take k*n time 
+
+        # instead merge 2 linked lists at a time, then merge 2 and so on
+        # so from k linked list we went to k/2 linked list to k/4 to k/8 until 1
+        #height of this tree is log k
+        #each layer have n, n/2, n/4 .... nodes
+        # so time complexiy is nlogk
+
+                
+        def mergeList(l1,l2):
+            dummy = ListNode()
+            tail = dummy 
+
+            while l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = l1
+                    l1 = l1.next
+                else:
+                    tail.next = l2
+                    l2 = l2.next 
+                tail = tail.next 
+            
+            if l1:
+                tail.next = l1
+            
+            if l2:
+                tail.next = l2
+            
+            return dummy.next
+
+        #edge case
+        if not lists or len(lists) ==0:
+            return None
+
+
+        #we want to merge lists until there's only one list  
+        while len(lists) > 1:
+            res = []
+            #iterate through pairs of linked list 
+            for i in range(0,len(lists),2):
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1 < len(lists) else None #next list only valid if it's in bounds 
+                #add merge sorted listed in the result 
+                res.append(mergeList(l1,l2))
+            lists = res 
+
+        return lists[0]
+
+        
