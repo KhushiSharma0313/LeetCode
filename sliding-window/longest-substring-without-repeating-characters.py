2e103b18c1d5630071abe24f5805ee31c61d9b3d
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-05-02 13:32:15 -0500

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/longest-substring-without-repeating-characters.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        #time - O(n), memory - O(n) due to set 
+        # string s, longest substring without duplicate
+        # substring - contiguous non empty set of string 
+        #e.g. = s "abcabcbb" output = 3
+        #we need substring so we can't sort them
+        # brute force - checking each substring, if it doesn't have duplicate, and return max 
+        # when we check substring starting at char a, if we find it has duplicate, we don't need to go any further
+        #use sliding window 
+        # we can use set for knowing duplicate, since it only contains unique character 
+        # 
+
+        #declare a set 
+        charSet = set()
+        res = 0
+        #left pointer starting at index 0
+        l = 0
+
+        #iterating through right pointer in string 
+
+        for r in range(len(s)):
+            if s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
+
+            res = max(res,len(charSet))
+        return res
