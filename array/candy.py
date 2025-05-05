fe2d4b7f49c24611cdd09113c8a0e25bfa6279e2
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-05-05 00:43:40 -0500

    LC-candy [Runtime: 13 ms; Memory: 19.9 MB]

---
+++ array/candy.py
@@ -0,0 +1,32 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        #child in line 
+        # rating value in array ratings 
+        # req - each child one candy 
+        # child with high rating than neghbour gets more candy 
+        # min number of candies 
+
+        #brute force would be checking each elements neighbour, then comparing if larger count of candy is increased
+        # for each element compare with its neighbours, then come back to left to fixmistakes
+        # so time = O(n^2) 
+        #
+        # first go from left to right and only check elements left neigbour and give candy accordingly 
+        # then go right to left, but check elements right neigbour and give candy if needed (if neigbour has more rating but not more candy)
+        # so time = O(n) and meory = O(1)
+
+        # for candy 
+        arr = [1]* len(ratings)
+
+        #first pass - comparing left neighbour, and going left to right 
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                arr[i] = 1 + arr[i-1]
+        
+        #second pass, going right to left, comparing right neigbours
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                arr[i] = max(arr[i], 1 + arr[i+1])
+
+        #return sum of the candies 
+        return sum(arr)
+        
