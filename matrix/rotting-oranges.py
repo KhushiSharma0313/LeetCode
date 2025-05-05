99ab5eb59baa41472522dcde52bd708266696ba3
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-05-04 22:58:02 -0500

    LC-rotting-oranges [Runtime: 4 ms; Memory: 17.6 MB]

--- matrix/rotting-oranges.py
+++ matrix/rotting-oranges.py
@@ -10,22 +10,50 @@ class Solution:
         # example -> 
         # 2, we check it's adj, if it's 1 we turn them to 2, and increase the count now again check 2 it's a
 
-        ROWS, COLS = len(grid), len(grid[0])
-        minute = 0
+        #if we use dfs, it's not gonna work cause it counts when each orange becomes rotten
+        # but oranges are rotten simultaneosly not one by one 
+        # we are using BFS, cause we can just count the layers
+        # since we're running bfs on multiple nodes, it's multi sources bfs 
+        # to know if its possible or not, keep track of fresh oranges before running bfs, then 
+        # then after bfs, if we have positive number of oranges, means it's not possible
+        #we're only gonna visit each node once, so time = O(n*m)
 
+        ROWS, COLS = len(grid), len(grid[0])
+        q = deque()
+        fresh = 0
+        time = 0
+
+        #iterating through each node 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 2:
-                    if r+1 >ROWS or r-1<0:
-                        break 
-                    if c+1> COLS or c-1<0:
-                        break
-                    grid[r+1][c] == 2 if grid[r+1][c] ==1 else 0
-                    grid[r-1][c] == 2 if grid[r-1][c] ==1 and r-1 >0 else 0
-                    grid[r][c+1] == 2 if grid[r][c+1] ==1 and c+1 < COLS else 0
-                    grid[r][c-1] == 2 if grid[r][c-1] ==1 and c-1 > 0 else 0
+                #check if it's fresh 
+                if grid[r][c] ==1:
+                    fresh +=1
+                #check if its rotten to run bfs on it 
+                if grid[r][c] ==2:
+                    q.append([r,c])
 
-                minute +=1
-        
-        return minute
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        # if queue is empty or fresh is empty, loop will not run 
+        while q and fresh >0:
+            # go through each element in the q 
+            for i in range(len(q)):
+                r,c = q.popleft()
+
+                #check its adjacent nodes
+                for dr, dc in directions:
+                    row = dr + r
+                    col = dc + c 
+
+                    # check if these coordinate has fresh oranges or it's out of bounds 
+                    if (row == ROWS or row<0 or col<0 or col == COLS or grid[row][col] !=1):
+                        continue 
+                    grid[row][col] = 2
+                    q.append([row,col])
+                    fresh -=1
+            time +=1
+
+        return time if fresh == 0 else -1 
+
         
