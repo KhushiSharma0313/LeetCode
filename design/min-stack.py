2e600ec9c8f0c118ecb4bdfeb42ce9fb81f2a157
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-05-05 23:37:05 -0500

    LC-min-stack [Runtime: 8 ms; Memory: 21.3 MB]

---
+++ design/min-stack.py
@@ -0,0 +1,52 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        #keeping track of min values
+        self.minStack = []
+        # initilizses stack object
+        #push function pushes val onto stack 
+        # pop removes value on top of stack 
+        # top gets top element of stack 
+        # getMins retrieves min element in stack constant time 
+
+        # use built in data strucure 
+        # everything stack can do apart fron getMin 
+        # brute force is look through each element = O(n)
+        # look at current minimum value for each element 
+        # so we'll have another stack with minimum value 
+        # so whenever pushing/popping we'll do it in both stacks to keep track of min 
+        # for getMin, look at top of min stack 
+        # implement stack with list 
+
+
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        #for min stack, determine which val is min, the val itself or top val of stack 
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        # return top value of min stack 
+        return self.minStack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
