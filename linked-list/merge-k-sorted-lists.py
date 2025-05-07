e320c6ccbff1e37539a6ab70a84fbee7138d1407
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-05-07 00:10:26 -0500

    LC-merge-k-sorted-lists [Runtime: 9 ms; Memory: 20.2 MB]

--- linked-list/merge-k-sorted-lists.py
+++ linked-list/merge-k-sorted-lists.py
@@ -1,3 +1,52 @@
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # approach is similar to merge sort 
+        # k => k/2 => k/4 ... 1 list (O(nlogn))
+        #helper function 
+        def mergeList(l1,l2):
+            dummy = ListNode()
+            curr = dummy 
+
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
+                curr = curr.next
+            
+            if l1:
+                curr.next = l1 
+                 
+
+            if l2:
+                curr.next = l2 
+                
+
+            return dummy.next 
+
+                #edge case
+        if not lists or len(lists) ==0:
+            return None
+        
+        # merge lists until 1  
+        while len(lists) >1:
+            res = []
+            for i in range(0,len(lists),2):
+                next_list = lists[i+1] if i+1 in range(len(lists)) else None
+                res.append(mergeList(lists[i], next_list))
+            
+            lists = res 
+        
+        return lists[0]
+
+
+            
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -21,47 +70,46 @@
         #each layer have n, n/2, n/4 .... nodes
         # so time complexiy is nlogk
 
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-                
-        def mergeList(l1,l2):
-            dummy = ListNode()
-            tail = dummy 
-
-            while l1 and l2:
-                if l1.val < l2.val:
-                    tail.next = l1
-                    l1 = l1.next
-                else:
-                    tail.next = l2
-                    l2 = l2.next 
-                tail = tail.next 
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
             
-            if l1:
-                tail.next = l1
+#         def mergeList(l1,l2):
+#             dummy = ListNode()
+#             tail = dummy 
+
+#             while l1 and l2:
+#                 if l1.val < l2.val:
+#                     tail.next = l1
+#                     l1 = l1.next
+#                 else:
+#                     tail.next = l2
+#                     l2 = l2.next 
+#                 tail = tail.next 
             
-            if l2:
-                tail.next = l2
+#             if l1:
+#                 tail.next = l1
             
-            return dummy.next
+#             if l2:
+#                 tail.next = l2
+            
+#             return dummy.next
 
-        #edge case
-        if not lists or len(lists) ==0:
-            return None
+#         #edge case
+#         if not lists or len(lists) ==0:
+#             return None
 
 
-        #we want to merge lists until there's only one list  
-        while len(lists) > 1:
-            res = []
-            #iterate through pairs of linked list 
-            for i in range(0,len(lists),2):
-                l1 = lists[i]
-                l2 = lists[i+1] if i+1 < len(lists) else None #next list only valid if it's in bounds 
-                #add merge sorted listed in the result 
-                res.append(mergeList(l1,l2))
-            lists = res 
+#         #we want to merge lists until there's only one list  
+#         while len(lists) > 1:
+#             res = []
+#             #iterate through pairs of linked list 
+#             for i in range(0,len(lists),2):
+#                 l1 = lists[i]
+#                 l2 = lists[i+1] if i+1 < len(lists) else None #next list only valid if it's in bounds 
+#                 #add merge sorted listed in the result 
+#                 res.append(mergeList(l1,l2))
+#             lists = res 
 
-        return lists[0]
+#         return lists[0]
 
         
