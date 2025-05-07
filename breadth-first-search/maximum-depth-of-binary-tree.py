2a29a064f36e75a0d08d44eed3cc20991e975f00
Khushi Sharma <sharmakhushi0313@gmail.com>
2025-05-06 23:38:16 -0500

    LC-maximum-depth-of-binary-tree [Runtime: 0 ms; Memory: 18.8 MB]

--- breadth-first-search/maximum-depth-of-binary-tree.py
+++ breadth-first-search/maximum-depth-of-binary-tree.py
@@ -1,3 +1,18 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #base case = length of root = 1 
+        # left subtree = max length 
+        # right subtree = max length 
+        # recursive = root base + max ( maxDepth(left), maxDpeth(right))
+        if not root:
+            return 0
+
+        max_length = 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+        return max_length
+
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,31 +20,7 @@
 #         self.left = left
 #         self.right = right
 
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        #bfs, where we're gonna traverse layer by layer and just count layers at the end 
-        #initialize queue and layer for bfs 
-        if not root:
-            return 0
-        q = deque([root])
-        layer = 0
 
-        while q: 
-            #iterating through each node in queue
-            for i in range(len(q)):
-                 # deleting the current node
-                node = q.popleft()
-                #if node has left subtree/node
-                if node.left:
-                    q.append(node.left)
-                #if node has right subtree/node
-                if node.right:
-                    q.append(node.right)
-            #increment layer after going through each node in the layer 
-            layer +=1
-           
-        
-        return layer
 
 
 # #recurisive dfs 
